387414279511882122f37b3b705605f0b75a330e
Jeremy William <jeremywilliam306@gmail.com>
2026-08-26 13:35:42 +1000

    sensors.js: adding the sensorIdsForCount that allows to fetch a valid PT-xx/TT-xx ids for the device repeatedly.

---
+++ safegauge assessment/src/Util/sensors.js
@@ -0,0 +1,8 @@
+export function sensorsIdsForCount(count) {
+    const ids = [];
+    for (let i = 0; i < count; i++) {
+        const prefix = i % 2 === 0 ? 'PT' : 'TT';
+        ids.push(`${prefix}-${String(i + 1).padStart(2, '0')}`);
+    }
+    return ids;
+}
